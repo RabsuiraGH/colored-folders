685236d78ff3731dcb5e967be8b777ed98596308
RabsuiraGH <rabsuira@proton.me>
2025-05-23 18:39:18 +0300

    Added test functionality

--- scripts/colorize-folders.js
+++ scripts/colorize-folders.js
@@ -1,4 +1,7 @@
-Hooks.on("renderSidebarTab", (app, html) => {
+Hooks.on("renderSidebarTab", (directory, html) => {
+  if (!html) return;
+  if (directory.tabName === "chat") return;
+
   const palette = [
     "#e63946",
     "#f1fa8c",
@@ -8,9 +11,23 @@ Hooks.on("renderSidebarTab", (app, html) => {
     "#ff6b6b"
   ];
 
-  html.find(".folder").each((i, el) => {
-    const color = palette[i % palette.length];
-    el.style.backgroundColor = color;
-    el.style.color = "#fff";
+  let randomizeButton = `
+    <a class="header-control" id="colorize-all-folders" title="Randomize All Folder Colours">
+        <i class="fas fa-dice"></i>
+    </a>`;
+  const search = html[0]?.querySelector(`input[name="search"]`);
+  search?.insertAdjacentHTML("afterend", randomizeButton);
+
+  html[0]?.querySelector("#colorize-all-folders")?.addEventListener("click", () => {
+    randomizeAllFolders(directory);
   });
 });
+
+async function randomizeAllFolders(directory) {
+    let updates = directory.folders.map((i) => {
+        const color = Color.fromHSV([Math.random(), Math.random() * 0.2 + 0.7, Math.random() * 0.2 + 0.7]).css;
+        return { _id: i.id, color: color };
+    });
+
+    await Folder.updateDocuments(updates);
+}
